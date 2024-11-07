95a2ad233df4e04613e669c91ee47d97aea64066
Bohdan Kisilchuk <newbite@promptlink.com>
2024-11-07 18:57:31 +0200

    commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         // init project
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
